745aac17e73c1e41145bd27e4eaa7f63035459b6
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2023-02-28 18:38:04 +0530

    stringify numbers

---
+++ Recursion/Medium-Hard/stringifyNumbers.js
@@ -0,0 +1,44 @@
+// stringifyNumbers
+// Write a function called stringifyNumbers which takes
+//  in an object and finds all of the values which are numbers
+//   and converts them to strings. Recursion would be a great way to solve this!
+
+const stringifyNumbers = (obj) => {
+	for (let key in obj) {
+		if (typeof obj[key] === 'number') {
+			obj[key] = `${obj[key]}`
+		} else if (typeof obj[key] === 'object') {
+			obj[key] = stringifyNumbers(obj[key])
+		}
+	}
+	return obj
+}
+
+let obj = {
+	num: 1,
+	test: [],
+	data: {
+		val: 4,
+		info: {
+			isRight: true,
+			random: 66,
+		},
+	},
+}
+
+console.log(stringifyNumbers(obj))
+
+// result
+/*
+{
+    num: "1",
+    test: [],
+    data: {
+        val: "4",
+        info: {
+            isRight: true,
+            random: "66"
+        }
+    }
+}
+*/
