e21762d2cca0cc99775429c2efe54c5a9066e845
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2023-03-06 11:41:55 +0530

    refactor

--- Searching/linearSearch.js
+++ Searching/linearSearch.js
@@ -6,16 +6,13 @@
 // Don't use indexOf to implement this function!
 
 function linearSearch(arr, num) {
-	let result = -1
 	for (let i = 0; i < arr.length; i++) {
 		if (arr[i] === num) {
-			result = i
-
-			return result
+			return i
 		}
 	}
 
-	return result
+	return -1
 }
 
 console.log(linearSearch([10, 15, 20, 25, 30], 15)) // 1
