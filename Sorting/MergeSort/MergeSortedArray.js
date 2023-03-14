919cf62afc551ec7d5ea28783da86df8e914f79b
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2023-03-14 10:48:51 +0530

    merge sort

---
+++ Sorting/MergeSort/MergeSortedArray.js
@@ -0,0 +1,46 @@
+//  Merge Sorted Array
+
+// You are given two integer arrays nums1 and nums2,
+// sorted in non-decreasing order, and two integers m and n,
+// representing the number of elements in nums1 and nums2 respectively.
+
+//  Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+//  The final sorted array should not be returned by the function, but
+//  instead be stored inside the array nums1. To accommodate this,
+//  nums1 has a length of m + n, where the first m elements denote the
+//  elements that should be merged, and the last n elements are set to
+//  0 and should be ignored. nums2 has a length of n.
+const mergeSort = (nums1, m, nums2, n) => {
+	const results = []
+	let i = 0
+	let j = 0
+
+	while (i < m && j < n) {
+		if (nums1[i] > nums2[j]) {
+			results.push(nums2[j])
+			j++
+		} else {
+			results.push(nums1[i])
+			i++
+		}
+	}
+
+	while (i < m) {
+		results.push(nums1[i])
+		i++
+	}
+	while (j < n) {
+		results.push(nums2[j])
+		j++
+	}
+
+	return results
+}
+var merge = function (nums1, m, nums2, n) {
+	nums1 = mergeSort(nums1, m, nums2, n)
+	console.log(nums1)
+	return
+}
+
+merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6, 9], 4)
